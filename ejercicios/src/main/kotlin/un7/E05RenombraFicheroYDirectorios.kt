6a49084ff1448b7b00ee41b6af6ca189184ac370
Edu <revilofe@gmail.com>
2022-02-13 16:30:15 +0100

    unidad 7. Ejemplos

--- ejercicios/src/main/kotlin/un7/E05RenombraFicheroYDirectorios.kt
+++ ejercicios/src/main/kotlin/un7/E05RenombraFicheroYDirectorios.kt
@@ -6,10 +6,10 @@ import kotlin.jvm.JvmStatic
 object E05RenombraFicheroYDirectorios {
     @JvmStatic
     fun main(args: Array<String>) {
-        val origenDir = File("C:/Temp/Fotos")
-        val destinoDir = File("C:/Temp/Media/Fotografies")
-        val origenDoc = File("C:/Temp/Media/Fotografies/Document.txt")
-        val destinoDoc = File("C:/Temp/Document.txt")
+        val origenDir = File("./Temp/Fotos")
+        val destinoDir = File("./Temp/Media/Fotografies")
+        val origenDoc = File("./Temp/Media/Fotografies/Document.txt")
+        val destinoDoc = File("./Temp/Document.txt")
         var res = origenDir.renameTo(destinoDir)
         println("Se ha movido y renombrado la carpeta? $res")
         res = origenDoc.renameTo(destinoDoc)
